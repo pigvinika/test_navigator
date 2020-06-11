718984523febbc62b351afa9623fd0a5e0bd88ec
pigvinika <pigvinika@gmail.com>
2020-06-12 00:33:31 +0300

    changed Procfile

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: bin/start-nginx gunicorn -c config/gunicorn.conf.py app:app
+web: bin/start-nginx gunicorn -c config/gunicorn.conf.py 'app:create_app()'
--- app.py
+++ app.py
@@ -3,6 +3,11 @@ from test_navigator import app, test_navigator_routes
 
 app.register_blueprint(test_navigator_routes.bp)
 
+
+def create_app():
+    return app
+
+
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
